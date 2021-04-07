aafb9e0cb31f06435ac0ddccdb2f50968688b9c2
Sunbabaara <csunbabaara@gmail.com>
2021-04-07 13:49:52 +0000

    added

--- server.js
+++ server.js
@@ -1,30 +1,60 @@
 const express=require("express");
+const dotenv= require("dotenv");
+const morgan = require("morgan");
+const results = require("./results");
 
-const students = require("./results")
+dotenv.config()
+
+
 const app=express();
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
 
-app.get("/", (req, res) =>{
-    res.send(" students results ");
+//single results
+app.get("/api/v1/results/:name", (req, res)=>{ 
+    res.json(results.filter((result) => result.name===req.params.name));
+})
+
+//post results
+app.post("/api/v1/results/:name", (req, res)=>{
+    const students={
+        name:req.body.name,
+        class:req.body.class,
+        marks:req.body.marks,
+    };
+    results.push(student);
+
+    res.json(results);
+})
+
+//delete single results
+app.delete("/api/v1/results/:name", (req, res)=>{ 
+    res.json(results.filter((result) => result.name!==req.params.name));
+})
+
+//update results
+app.put("/api/v1/results/:name", (req, res)=>{ 
+    const result=results.some((result)=>result.name===req.params.name);
+    resultFound &&
+        results.forEach((result)=> {
+            result.name===req.params.name &&
+                result.name===req.body.name
+                result.community===req.body.name
+            
+        });
+    
+    res.json(results)
 });
 
-app.get("/api/students", (req, res) =>{res.json(students);
-});
 
-app.delete("/api/students/:name", (req, res) =>{
-    const name= req.params.name;
-    const results=results.some((r)=>r.name===name);
-   
-    if (results){
-    res.json({
-        msg: `results  ${name}`,
-        results: results.filter((r)=>r.name!==name),
-});
-    }
-});
-app.post("/api/students", (req, res) =>{res.send("this is post route");
-});
+//get all results
+app.get("/api/v1/results", (req, res)=>{
+    res.json(results);
+})
+ 
+const PORT = process.env.PORT||5003;
 
-app.put("/api/students", (req, res) =>{res.send("this is put route");
-});
+app.listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
 
-app.listen(5002, ()=> console.log('server is running on port ${5002}'));
+
